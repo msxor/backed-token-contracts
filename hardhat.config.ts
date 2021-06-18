75ab44199c5bfd2289f0924c55626f5b528cb3e9
Maxim <maxim.feofilov@gmail.com>
2021-06-18 22:54:46 +0300

    Ini project

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import { task } from 'hardhat/config';
+import "@nomiclabs/hardhat-waffle";
+
+import { HardhatRuntimeEnvironment, HardhatUserConfig } from 'hardhat/types';
+
+task("accounts", "Prints the list of accounts", async (args, hre: HardhatRuntimeEnvironment) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 5000
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+      chainId: 4,
+      url: `https://rinkeby.infura.io/v3/bc4d11088848481a972333f5161120fe`,
+      accounts:   {
+        mnemonic: `absurd anchor bullet lobster unable exclude weird lucky bar soda dumb first`
+      },
+      gasPrice: 2000000000,
+    }
+  }
+}
+
+export default config
